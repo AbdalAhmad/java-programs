324d06327593ec38da2fc59ac4f22258770d8c2f
Lucky <abdalraza251@gmail.com>
2023-10-16 09:16:39 +0530

    pattern added and basic updated

--- basics/basic.java
+++ basics/basic.java
@@ -1,1 +1,30 @@
-//Abdal is good businessman
+//Abdal  is good businessman
+// public class basic{
+//     public static void main (String  args[]){
+        
+//     }
+// } this is boiler plate code it will seen again again ..
+//  public class basic{
+//     public static void main (String  args[]){
+//         System.out.print("Hello  Abdal ");  // print is for a single line it will print in countinous way  a one line
+
+//     }
+//     }
+
+//  public class basic{
+//     public static void main (String  args[]){
+//          System.out.println("Hello  Abdal ");  
+//          System.out.println("Hello Abdal ");    //println give a space of a line 
+//          System.out.println("Hello Abdal ");  //println make each output in a new line 
+//          System.out.println("Hello  Lucky ");  
+//     }
+//     }
+
+//  public class basic{
+//     public static void main (String  args[]){
+//          System.out.print("Hello  Abdal\n ");  // n pheeche se new line ka space start hoga 
+//          System.out.print("Hello Abdal \n");    //n give a space of a line 
+//          System.out.println("Hello Abdal \n");  //n make each output in a new line 
+//          System.out.println("Hello  Lucky \n");  //ln and n give 2 space if both are used in one 
+//     }
+//     }
---
+++ basics/pattern.java
@@ -0,0 +1,7 @@
+  public class pattern{
+    public static void main (String  args[]){
+         System.out.println("Hello  Abdal ");  
+         System.out.println("Hello Abdal ");   
+         System.out.println("Hello  Lucky ");  
+    }
+    }
