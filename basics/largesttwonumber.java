e3847da586616a95a44492bb33fe9ce130a79a91
Lucky <abdalraza251@gmail.com>
2023-10-18 16:12:17 +0530

    Updated

---
+++ basics/largesttwonumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class largesttwonumber{
+public static void main(String[] args){
+    Scanner abdal = new Scanner(System.in);
+    int Abdal =  abdal.nextInt();
+     int Raja =  abdal.nextInt();
+if(Abdal>Raja){
+System.out.println("Abdal  is bigger in age then Raja");
+}
+if(Abdal == Raja){
+System.out.println("Abdal   age is same as Raja");
+}
+else{
+System.out.println("Abdal is younger then Raja");
+
+}
+}
+}
---
+++ basics/numisevenorodd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class numisevenorodd{
+public static void main(String[] args){
+    Scanner abdal = new Scanner(System.in);
+    int number =  abdal.nextInt();
+  
+if(number %2 == 0){
+System.out.println("number is even ");
+}
+else{
+System.out.println("number is not even ");
+
+}
+}
+}
