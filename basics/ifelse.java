ff870ddf900a3991a83a47414f288994e701c0d2
Lucky <abdalraza251@gmail.com>
2023-10-18 15:44:02 +0530

    if else added

---
+++ basics/ifelse.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ifelse{
+public static void main(String[] args){
+    Scanner abdal = new Scanner(System.in);
+    int age =  abdal.nextInt();
+if(age>=18){
+System.out.println("you are eligible for marriage");
+}else{
+System.out.println("you are not eligible for marriage");
+
+}
+}
+}
