68bbd5032e3c756173782b54676c53a9ec14c592
Lucky <abdalraza251@gmail.com>
2023-10-18 15:50:50 +0530

    updated in if else

--- basics/ifelse.java
+++ basics/ifelse.java
@@ -6,7 +6,11 @@ public static void main(String[] args){
     int age =  abdal.nextInt();
 if(age>=18){
 System.out.println("you are eligible for marriage");
-}else{
+}
+if (age>13 || age<18){  // in this operator equal sign is not used
+System.out.println("you are eligible but have to wait in marriage ");
+}
+else{
 System.out.println("you are not eligible for marriage");
 
 }
