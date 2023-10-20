235c17f5e265274d3773237ea29542a4c2c8155e
Lucky <abdalraza251@gmail.com>
2023-10-20 19:09:09 +0530

    Aded

---
+++ basics/characterpattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+public class characterpattern{
+public static void main (String  args []){
+   int n = 7;
+   char ch = 'A';
+
+for(int line = 1; line <=n ; line++ ){  // 4 can be take as a user // line
+    for(int j = 1; j<= line; j++){  // star
+        System.out.print(ch);
+        ch++;
+    }
+    System.out.println(""); // for each line space 
+}
+
+}
+
+}
