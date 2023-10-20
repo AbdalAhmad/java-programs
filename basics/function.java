4623e61f57d8849c07eb4973bae6b10ea3866f53
Lucky <abdalraza251@gmail.com>
2023-10-20 19:48:11 +0530

    Added

---
+++ basics/function.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class function {
+    public static void print(){
+        System.out.println("Hello Abdal");
+    }
+    public static void main (String [] args){
+        print();
+    }
+}
