c516b47c7673142c490a8f75ef488edbff823743
Lucky <abdalraza251@gmail.com>
2023-10-20 19:02:54 +0530

    added pattern more

---
+++ basics/halfpyramid.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class halfpyramid{
+public static void main (String  args []){
+   int n = 6;
+
+for(int line = 1; line <=n ; line++ ){  // 4 can be take as a user // line
+    for(int j = 1; j<= line; j++){  // star
+        System.out.print(j);
+    }
+    System.out.println(""); // for each line space 
+}
+
+}
+
+}
