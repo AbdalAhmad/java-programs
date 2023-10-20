231df190c53d57615236be793a914f5f06b17a89
Lucky <abdalraza251@gmail.com>
2023-10-20 18:46:04 +0530

    Added

--- basics/invertedstar.java
+++ basics/invertedstar.java
@@ -1,13 +1,12 @@
-
-
 import java.util.*;
 
 
-public class patternone{
+public class invertedstar{
 public static void main (String  args []){
-   
-for(int line = 1; line <=4 ; line++ ){
-    for(int star = 1; star<= line ; star++){
+   int n = 6;
+
+for(int line = 1; line <=n ; line++ ){  // 4 can be take as a user
+    for(int star = 1; star<= (n-line+1); star++){
         System.out.print("*");
     }
     System.out.println(""); // for each line space 
