4f86f936c08c08bee5586c8faeccfe81bf4dd8cd
Lucky <abdalraza251@gmail.com>
2023-10-27 23:21:44 +0530

    ADDED

--- basics/kadansmax.java
+++ basics/kadansmax.java
@@ -4,7 +4,13 @@ public class prefixsumsubarray{
     public static void kadanes(){
         int ms = Integer.MIN_VALUE;
         int cs = 0;
-        
+     for(int i =0; i<number.length ;i++){
+        cs = cs+number[i];
+        if(cs<0){
+            cs =0;
+        }
+        ms = Math.max(cs,ms); // jo bhi max hoga cs or max mein se  vo ms mein chale jayega
+     }
     }
     public static void  main(String [] args){
     int number [] = {2,4,6,8 ,10};
@@ -15,23 +21,5 @@ public class prefixsumsubarray{
 
 
 
-// public static void pairs(int number []){
-    //     int currentsum = 0;
-    //     int maxsum = Integer.MIN_VALUE;
-    //     int prefix [] = new int[number.length];
-    //     prefix[0] = number [0];  
 
-    //     for(int i = 1; i<prefix.length; i++){ 
-    //         prefix [i] = prefix[i-1]+number[i]; 
-    //     }
-    //    for(int i =0 ; i <number.length ;i++){     
-    //     for(int j = i  ; j<number.length ; j++){ 
-    //         currentsum = i == 0 ?  prefix[j] : prefix[j]- prefix[i-1];   
-           
-    //          if(maxsum<currentsum){
-    //             maxsum = currentsum;
-    //          } 
-    //     }
-    //     }
-    //    System.out.println("maximum sum of array is  : "+maxsum);
-    // }
+  
