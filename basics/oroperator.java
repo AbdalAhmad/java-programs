6c6d0216d450e89246e70ce3b59d82618798439f
Lucky <abdalraza251@gmail.com>
2023-10-30 06:58:46 +0530

    xor

---
+++ basics/oroperator.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class oroperator{
+    public static void main(String args []){
+   int a =5;
+   int n= 6;
+   int c = 5 | 6;
+   System.out.println(c);
+    }
+}
