ba651189edfe4bffb2da36565ca5dc737c119dc5
Lucky <abdalraza251@gmail.com>
2023-10-31 22:39:58 +0530

    added

--- basics/countingsort.java
+++ basics/countingsort.java
@@ -33,7 +33,7 @@ public class countingsort {
   }
 
   public static void main(String args[]) {
-   int  arr[] = { 5, 4, 1, 3, 2 };
+   int  arr[] = { 5, 4, 1, 1,  3, 2 };
 
 countingsort(arr);
     printa(arr);  
