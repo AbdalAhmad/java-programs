8b8e4b70102b8c400cf278bcf6f5aa7034408870
Lucky <abdalraza251@gmail.com>
2023-10-31 10:24:55 +0530

    new

---
+++ basics/addinteger.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class addinteger {
+
+  public static void main(String args[]) {
+    int x = 6;
+    int y = 4;
+
+    System.out.println(x + " is " + -~x); // x=x+1
+    System.out.println(y + " is " + -~y); //
+  }
+}
---
+++ basics/upperlowercase.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class upperlowercase {
+
+  public static void main(String args[]) {
+    // Convert uppercase character to lowercase
+  for(char ch = 'A'; ch <= 'Z'; ch++){
+     System.out.println((char)(ch|" ")); 
+  // prints abcdefghijklmnopqrstuvwxyz
+  }
+   
+  }
+}
