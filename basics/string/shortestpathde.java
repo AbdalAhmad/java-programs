132679f265140239fe018fe62e8cf3886627b5c4
Lucky <abdalraza251@gmail.com>
2023-11-02 17:57:22 +0530

    added get shortest direction

---
+++ basics/string/shortestpathde.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.lang.Math.*;
+
+public class shortestpathde {
+    public static float getShortestpath(String path) {
+        int x = 0, y = 0;
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            // south
+            if (dir == 'S') {
+                y--;
+            }
+            // north
+            else if (dir == 'N') {
+                y++;
+            }
+            // west
+            else if (dir == 'W') {
+                x--;
+            }
+            // east
+            else {
+                x++;
+            }
+        }
+        int x2 = x * x;
+        int y2 = y * y;
+        return (float) Math.sqrt(x2 + y2);
+    }
+
+    public static void main(String args[]) {
+        String path = "WNEENESENNN";
+        System.out.println(getShortestpath(path));
+    }
+}
