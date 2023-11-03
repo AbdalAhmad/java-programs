e8f5f2ccb7e6411f70204ffe61359da0e62a2d66
Lucky <abdalraza251@gmail.com>
2023-11-03 20:30:54 +0530

    oops

--- basics/oops/copyconstructor.java
+++ basics/oops/copyconstructor.java
@@ -10,13 +10,14 @@ public class copyconstructor{
     a.marks[1] = 80;
     a.marks[2] = 90;
 
- for(int i = 0; i< 3 ; i++){
-        System.out.println(a.marks[i]);
-    }
+//  for(int i = 0; i< 3 ; i++){
+//         System.out.println(a.marks[i]);
+//     }
+
     Student b = new Student(a);
     b.password = "xyz";
-    b.marks[2] = 100; // update  if i use a or b object both give updated value 
-    for(int i = 0; i< 3 ; i++){
+    a.marks[2] = 100; // update  if i use a or b object both give updated value  in shalllow copy 
+    for(int i = 0; i< 3 ; i++){ // in deep copy a.marks[2] is not change  in b 
         System.out.println(b.marks[i]);
     }
     }
@@ -26,14 +27,26 @@ class Student {
     int roll;
     String password;
     int marks[];
-    //copy
-    Student(Student c){ // i can use a or any other like c  i used
-      
-        this.name = c.name;
-        this.roll = c.roll;
-        this.marks = c.marks;
+    //copy// i can use a or any other like c  i used
 
+    // shallow copy    constructor
+    // Student(Student c){ 
+    //     this.name = c.name;
+    //     this.roll = c.roll;
+    //     this.marks = c.marks;  // reference copy hogai hai
+    // }
+
+    // deep copy constructor
+    Student(Student a){
+        this.name = a.name;
+        this.roll = a.roll;
+        marks = new int [3];
+        for(int i =0; i<marks.length ; i++){
+            this.marks[i] = a.marks[i];  // deep copy ek ek krke copy hogi hain
+        }
     }
+      
+
     Student(){
         marks = new int [3];
         System.out.println("constructor is called...");
