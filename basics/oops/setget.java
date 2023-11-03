01d3f0e2ca93930ddbb62a1d0285639c553965e3
Lucky <abdalraza251@gmail.com>
2023-11-03 11:31:24 +0530

    oops

---
+++ basics/oops/setget.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class setget{
+
+ public static void  main(String args[]){
+   Pen a1 = new Pen();
+
+   a1.setColor("Blue"); // here set it value is  passed
+   System.out.println(a1.getColor()); // call it from here
+   
+  a1.setColor("Yellow");
+   System.out.println(a1.getColor()); 
+ }
+}
+
+class Pen{
+    private String color;
+
+    String getColor(){
+        return this.color;
+    }  //get color from private
+
+    void setColor(String newColor){
+        this.color = newColor;
+    } // here set it 
+}
