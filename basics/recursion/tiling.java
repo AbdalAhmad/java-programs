d1c385241437643f6f9197d91c86aa8145fb738d
Lucky <abdalraza251@gmail.com>
2023-11-04 19:23:09 +0530

    4*1 also

---
+++ basics/recursion/tiling.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class tiling{
+    public static int tiling(int n){
+        if(n==0||n==1){
+            return 1; // for 0 1 n there is only 1 ways
+        }
+        int fnm1= tiling(n-1);  // vertical ways
+        int fnm2 = tiling(n-2);// horizontal ways
+        int ways = fnm1+ fnm2;
+        return ways;
+    }
+    public static void main(String args[]){
+     System.out.println(tiling(4));
+    }
+}
