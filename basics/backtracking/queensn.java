1fa37c74032c1f80b36e1c02453d661d4bb9b60e
Lucky <abdalraza251@gmail.com>
2023-11-10 17:14:14 +0530

    added

---
+++ basics/backtracking/queensn.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class queensn {
+    public static void isSafe(char boards [][] , int row , int col){
+        // vertical up see
+        for(int i = row-1; i>= 0; i--){
+            if(boards[i][col]=='Q'){
+                return false;
+            }
+        }
+        // diagonal left up
+        for(int i = row-1, j= col-1 ; i>= 0 && j>=0; i-- , j--){
+        if(boards[i][j]=='Q'){
+            return false;
+
+        }
+        }
+        // diagonal right up
+        for(int i = row-1, j = col+1; i>=0 && j<boards.length; i-- , j++ ){
+            if(boards [i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void nQueens(char boards [][], int row ){
+        //base
+        if(row== boards.length){
+            printBoard(boards);
+            return;
+        }
+        // cloumn loop
+        if(isSafe(boards, row, j)){
+             for(int j = 0; j<boards.length ; j++){
+            boards[row][j] = 'Q';
+            nQueens(boards, row+1); // function call
+            boards[row][j] = '.'; // backtracking step
+        }
+        }
+       
+
+    }
+    public static void printBoard(char boards [][]){
+        System.out.println("----------chessboards---------");
+        for(int i = 0; i<boards.length; i++){
+            for(int j =0; j<boards.length ; j++){
+                System.out.print(boards[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String args []){
+    int n = 4;
+    char boards [][] = new char[n][n];
+    // intialize
+    for(int i =0; i<n ; i++){
+        for(int j =0 ; j<n ; j++){
+            boards [i][j]= '.';
+        }
+    }
+    nQueens(boards, 0);
+    }
+}
---
+++ basics/recursion/binarystring.java
@@ -0,0 +1,15 @@
+// print all the binary string of size N without consecutive ones
+
+// one not included 
+import java.util.Scanner;
+
+public class binarystring{
+    public static void printbin(int n , int lastplace, String str){
+        if(lastplace == 0){
+            printbin(n-1)
+        }
+    }
+    public static void main(String args[]){
+
+    }
+}
