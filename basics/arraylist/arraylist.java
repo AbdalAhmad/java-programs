ef5c8f1effdf423d3dda6e3b2f63ba72c0d6c3c2
Lucky <abdalraza251@gmail.com>
2023-11-11 23:28:15 +0530

    Added

---
+++ basics/arraylist/arraylist.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class arraylist{
+    public static void main(String args []){
+        // ClassName objectName = new ClassName();
+        // Syntax
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<String> list3 = new ArrayList<>();
+    }
+}
