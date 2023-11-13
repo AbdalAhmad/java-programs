7d82fdaf2b3111e4ea959d044b88f43ce8875eeb
Lucky <abdalraza251@gmail.com>
2023-11-13 16:28:54 +0530

    Added

--- basics/arraylist/containerwithwater.java
+++ basics/arraylist/containerwithwater.java
@@ -1,18 +1,39 @@
 import java.util.*;
 
 public class containerwithwater{
-    public static int storewater(ArrayList<Integer> height){
+    // public static int storewater(ArrayList<Integer> height){
+    //     int maxWater = 0;
+    //     // brute force
+    //     for(int i = 0; i<height.size(); i++){
+    //         for(int j = i+1 ; j<height.size(); j++){
+    //             int hi = Math.min(height.get(i), height.get(j));
+    //             int width = j-i;
+    //             int currWater = hi * width;
+    //             maxWater = Math.max(maxWater, currWater);
+    //         }
+    //     }
+    //  return maxWater;
+    // }
+
+    // 2 pointer approach
+    public static  int storewater(ArrayList<Integer> height){
         int maxWater = 0;
-        // brute force
-        for(int i = 0; i<height.size(); i++){
-            for(int j = i+1 ; j<height.size(); j++){
-                int hi = Math.min(height.get(i), height.get(j));
-                int width = j-i;
-                int currWater = hi * width;
-                maxWater = Math.max(maxWater, currWater);
+        int lp = 0;
+        int rp = height.size()-1;
+        while(lp<rp){
+            //calculate water area
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int width = rp-lp;
+            int currWater = ht*width;
+            maxWater = Math.max(maxWater, currWater);
+            // update ptr
+            if(height.get(lp) < height.get(rp)){
+                lp++;
+            }else{
+                rp--;
             }
         }
-     return maxWater;
+        return maxWater;
     }
     public static void main(String args []){
     ArrayList<Integer> height = new ArrayList<>();
@@ -28,6 +49,7 @@ public class containerwithwater{
     height.add(3);
     height.add(7);
 
-   System.out.print(height);
+   System.out.println(height);
+   System.out.println(storewater(height));
     }
 }
