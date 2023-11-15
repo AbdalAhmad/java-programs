fc8ff287b08811d34bcce965db6ae82145075365
Lucky <abdalraza251@gmail.com>
2023-11-15 18:19:01 +0530

    Added

--- basics/linked_list/linked.java
+++ basics/linked_list/linked.java
@@ -11,10 +11,26 @@ public class linked{
     }
     public static Node head;
     public static Node tail;
+    public void addFirst(int data){
+     // step 1 = create new node
+     Node newNode = new Node(data);
+     if(head == null){
+    head = tail = newNode;
+    return;
+     }
+     // step2 -  newNode next = head
+     newNode.next = head; // link
+     // step 3
+     head = newNode;
+
+
+    }
 
     public static void main(String args[]){
    Linked ll = new LinkedList();
    ll.head = new Node(1);
    ll.head.next = new Node(2);
+   ll.addFirst(1);
+   ll.addFirst(2);
     }
 }
