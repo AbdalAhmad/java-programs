cd1a532b79ef4c1ee9313a1468c6588c5398e50b
Lucky <abdalraza251@gmail.com>
2023-11-29 15:24:48 +0530

    added

--- basics/linked_list/palindrome.java
+++ basics/linked_list/palindrome.java
@@ -88,7 +88,7 @@ public Node findMid(Node head){
     ll.addFirst(2);
     ll.addFirst(2);
     ll.addFirst(3);
-/// aded
+// not added 
     ll.print();
 System.out.println(ll.checkPalindrome());
     
